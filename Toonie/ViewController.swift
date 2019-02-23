c965217292c872c61a9108416ef17fa699ffb0cb
Jae-eun <je4297@naver.com>
2019-02-24 02:42:56 +0900

    Initial Commit

---
+++ Toonie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toonie
+//
+//  Created by 이재은 on 24/02/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ ToonieTests/ToonieTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToonieTests.swift
+//  ToonieTests
+//
+//  Created by 이재은 on 24/02/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import XCTest
+@testable import Toonie
+
+class ToonieTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
