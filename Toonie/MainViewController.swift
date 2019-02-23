9da8ae8ad9895d51e6bb5c8068fc679c78e1fa2d
Jae-eun <je4297@naver.com>
2019-02-24 04:21:13 +0900

    first commit

---
+++ Toonie/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Toonie
+//
+//  Created by 이재은 on 24/02/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
