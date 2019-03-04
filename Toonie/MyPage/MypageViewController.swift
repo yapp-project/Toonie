40ad65df2f89149199e6bdd9d3a305331e1f47c7
eojin <eozinee@gmail.com>
2019-03-04 17:52:59 +0900

    test2

--- Toonie/MyPage/MypageViewController.swift
+++ Toonie/MyPage/MypageViewController.swift
@@ -13,6 +13,7 @@ class MypageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
     
