29095b8ddaae660435f465d4d328589936a1a609
Jae-eun <je4297@naver.com>
2019-03-14 05:14:31 +0900

    setting tab 삭제, tabbar image 적용

--- Toonie/Main/MainViewController.swift
+++ Toonie/Main/MainViewController.swift
@@ -9,25 +9,46 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-  // 100 Feed, 101 Look, 102 MyPage, 103 Setting
+  // 100 Feed, 101 Look, 102 MyPage
   @IBOutlet weak var feedContainerView: UIView!
   @IBOutlet weak var lookContainerView: UIView!
   @IBOutlet weak var myPageContainerView: UIView!
-  @IBOutlet weak var settingContainerView: UIView!
-
+  @IBOutlet weak var homeButton: UIButton!
+  @IBOutlet weak var searchButton: UIButton!
+  @IBOutlet weak var myButton: UIButton!
+  
   @IBAction func tabBarButtonDidTap(_ sender: UIButton) {
-    self.hideContainerView(viewTag: sender.tag - 100)
+    hideContainerView(viewTag: sender.tag - 100)
+    changeTabbarButtonState(viewTag: sender.tag)
   }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-        self.view.viewWithTag(100)?.isHidden = false
+    self.view.viewWithTag(100)?.isHidden = false
+    homeButton.isSelected = true
   }
 
   /// 컨테이너뷰 태그값에 따라서 숨김
   func hideContainerView(viewTag: Int) {
-    for index in 100..<104 {
+    for index in 100..<103 {
       self.view.viewWithTag(index)?.isHidden = (viewTag != index ? true : false)
+    }
+  }
+  
+  /// 태그값에 따라서 버튼 상태 변경
+  func changeTabbarButtonState(viewTag: Int) {
+    homeButton.isSelected = false
+    searchButton.isSelected = false
+    myButton.isSelected = false
+    switch viewTag {
+    case 200:
+      homeButton.isSelected = true
+    case 201:
+      searchButton.isSelected = true
+    case 202:
+      myButton.isSelected = true
+    default:
+      break
     }
   }
 }
