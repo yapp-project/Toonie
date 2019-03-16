6524b22ab5b232f9a41a4e9c8bb942c15a23a92f
ebpark9511 <ebpark9511@gmail.com>
2019-03-16 16:25:45 +0900

    함수명 변경

--- Toonie/Splash/SplashViewController.swift
+++ Toonie/Splash/SplashViewController.swift
@@ -19,34 +19,34 @@ final class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
  
-        logoAnimationViewSet()
+        setLogoAnimationView()
         
         logoAnimationView?.play { (finished) in
             if finished {
                 //키워드 선택해야하는 유저라면
-                self.keywordMoveBtnShow()
+                self.moveKeywordView()
                 //아니라면 일반화면으로 이동
-//                self.mainMove()
+//                self.moveMainView()
             }
         }
     }
   
     ///애니메이션 후 메인화면으로 이동
-    func mainMove() {
+    func moveMainView() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "RootViewController")
         UIApplication.shared.keyWindow?.rootViewController = viewController
     }
     
     ///키워드 화면 진입
-    func keywordMoveBtnShow() {
+    func moveKeywordView() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "KeywordSelectViewController")
         UIApplication.shared.keyWindow?.rootViewController = viewController
     }
     
     ///logoAnimationView 세팅
-    func logoAnimationViewSet() {
+    func setLogoAnimationView() {
         logoAnimationView = LOTAnimationView(name: "logo") 
         logoAnimationView!.contentMode = .scaleAspectFill
         logoAnimationView!.frame = CGRect.init(x: 0,
