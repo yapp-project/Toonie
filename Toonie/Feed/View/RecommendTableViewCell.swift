c1a37bf6d8ef9d47518c60c0729a070a61541c38
Jae-eun <je4297@naver.com>
2019-03-18 13:47:14 +0900

    '지금 나는' 상세 화면 -> 은비에게 토스~

---
+++ Toonie/Feed/View/RecommendTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  RecommendTableViewCell.swift
+//  Toonie
+//
+//  Created by 이재은 on 18/03/2019.
+//  Copyright © 2019 Yapp. All rights reserved.
+//
+
+import UIKit
+
+// '지금 나는' 추천 테이블뷰셀
+final class RecommendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
