92954d11221a863d68f8599d4304afc23f4f86e2
박은비 <ebpark9511@gmail.com>
2019-03-21 18:42:12 +0900

    피드 상세

---
+++ Toonie/Feed/RecommendView/RecommendCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  RecommendCollectionViewCell.swift
+//  Toonie
+//
+//  Created by 박은비 on 21/03/2019.
+//  Copyright © 2019 Yapp. All rights reserved.
+//
+
+import UIKit
+
+// '지금나는
+final class RecommendCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var recentToonImageView: UIImageView!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var bookMarkButton: UIButton!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        recentToonImageView.image = nil
+        artistLabel.text = nil
+        bookMarkButton.isSelected  = false
+    }
+    
+    // MARK: - Functions
+    
+    /// 컬렉션뷰셀 데이터 설정
+    func setRecommendCollectionViewCellProperties() {
+        recentToonImageView.image = UIImage(named: "sample2")
+        artistLabel.text = "임유끼"
+    }
+}
