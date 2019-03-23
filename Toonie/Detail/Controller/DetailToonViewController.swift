481011545e35ae59664adf1d8a886bd5bf7b0f9d
Jae-eun <je4297@naver.com>
2019-03-24 01:55:21 +0900

    Add Detail StoryBoard

---
+++ Toonie/Detail/Controller/DetailToonViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailToonViewController.swift
+//  Toonie
+//
+//  Created by 이재은 on 23/03/2019.
+//  Copyright © 2019 Yapp. All rights reserved.
+//
+
+import UIKit
+
+// 인스타툰 상세 화면
+final class DetailToonViewController: UIViewController {
+
+  // MARK: - Life Cycle
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
