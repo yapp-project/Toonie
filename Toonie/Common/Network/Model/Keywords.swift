9e950a631406025ba32c85543ef6965a536c726d
박은비 <ebpark9511@gmail.com>
2019-03-31 04:11:07 +0900

    통신관련 커밋..

---
+++ Toonie/Common/Network/Model/Keywords.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Toonie
+//
+//  Created by 박은비 on 31/03/2019.
+//  Copyright © 2019 Yapp. All rights reserved.
+//
+
+import Foundation
+struct Keywords: Codable {
+    let success: Bool?
+    let keywords: [String]?
+}
+ 
