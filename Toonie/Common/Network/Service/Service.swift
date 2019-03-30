8ee10c706ea6c70f3e8b028d0f6d76e6dc5bd878
eojin <eozinee@gmail.com>
2019-03-31 06:12:43 +0900

    Modify Views

--- Toonie/Common/Network/Service/Service.swift
+++ Toonie/Common/Network/Service/Service.swift
@@ -1,10 +1,12 @@
 import Foundation
 
-struct TokenService : Requestable {
+struct TokenService: Requestable {
     typealias NetworkData = Token
     static let shareInstance = TokenService()
     
-    func getToken(url : String, params : [String : Any]? = nil, completion : @escaping (NetworkResult<Any>) -> Void) {
+    func getToken(url: String,
+                  params: [String : Any]? = nil,
+                  completion: @escaping (NetworkResult<Any>) -> Void) {
         get(url, params: params) { (result) in
             
             switch result {
