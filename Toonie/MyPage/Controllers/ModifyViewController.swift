acabd3fcc9c8d55a7cd0b0f7a1d84ee459195cf6
eojin <eozinee@gmail.com>
2019-03-31 02:13:42 +0900

    Create ModifyViewController

---
+++ Toonie/MyPage/Controllers/ModifyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ModifyViewController.swift
+//  Toonie
+//
+//  Created by 양어진 on 31/03/2019.
+//  Copyright © 2019 Yapp. All rights reserved.
+//
+
+import UIKit
+
+class ModifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Toonie/MyPage/Views/ModifyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ModifyCollectionViewCell.swift
+//  Toonie
+//
+//  Created by 양어진 on 31/03/2019.
+//  Copyright © 2019 Yapp. All rights reserved.
+//
+
+import UIKit
+
+class ModifyCollectionViewCell: UICollectionViewCell {
+    
+}
