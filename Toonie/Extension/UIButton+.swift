cf7aa07ece3a93c8001c7b4044084633fb89958a
박은비 <ebpark9511@gmail.com>
2019-05-07 13:20:34 +0900

    Bottom Tabbar center fix

--- Toonie/Extension/UIButton+.swift
+++ Toonie/Extension/UIButton+.swift
@@ -8,24 +8,23 @@
 
 import UIKit
 
-
 extension UIButton {
     func centerImageAndButton(_ gap: CGFloat, imageOnTop: Bool) {
         
         guard let imageView = self.imageView,
             let titleLabel = self.titleLabel else { return }
         
-        let sign: CGFloat = imageOnTop ? 1 : -1;
-        let imageSize = imageView.frame.size;
+        let sign: CGFloat = imageOnTop ? 1 : -1
+        let imageSize = imageView.frame.size
         self.titleEdgeInsets = UIEdgeInsets.init(top: (imageSize.height+gap)*sign,
                                                  left: -imageSize.width,
                                                  bottom: 0,
-                                                 right: 0);
+                                                 right: 0)
         
-        let titleSize = titleLabel.bounds.size;
+        let titleSize = titleLabel.bounds.size
         self.imageEdgeInsets = UIEdgeInsets.init(top: -(titleSize.height+gap)*sign,
                                                  left: 0,
                                                  bottom: 0,
-                                                 right: -titleSize.width);
+                                                 right: -titleSize.width)
     }
 }
